c3c660d3a017988ffa3531ebd8fc220d47ef42e1
Arjin Kavalcı <arjin.33@outlook.com>
2025-02-26 14:50:18 +0300

    update build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.etransportapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.etransportapp"
