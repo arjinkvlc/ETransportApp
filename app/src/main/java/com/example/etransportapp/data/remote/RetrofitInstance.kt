749f3aa95a20965103f832f0d0034a6e70cdbec8
Umut Kalelioğlu <72888847+umutkaleli@users.noreply.github.com>
2025-05-23 19:38:31 +0300

    OkHttpClient added

--- app/src/main/java/com/example/etransportapp/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/etransportapp/data/remote/RetrofitInstance.kt
@@ -1,16 +1,41 @@
 package com.example.etransportapp.data.remote
 
 import com.example.etransportapp.data.remote.api.UserApi
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://10.0.2.2:5085/"
+    private const val BASE_URL = "https://evrenblackbird.com/"
 
+    fun getUnsafeOkHttpClient(): OkHttpClient {
+        val trustAllCerts = arrayOf<TrustManager>(
+            object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            }
+        )
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+        val sslSocketFactory = sslContext.socketFactory
+
+        return OkHttpClient.Builder()
+            .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            .hostnameVerifier { _, _ -> true }
+            .build()
+    }
 
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(getUnsafeOkHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
