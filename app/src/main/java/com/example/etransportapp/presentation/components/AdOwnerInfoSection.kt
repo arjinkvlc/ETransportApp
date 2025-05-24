7c72825745829c6d476b3d6b68205fabbcd29dc6
Arjin Kavalcı <arjin.33@outlook.com>
2025-05-24 18:35:07 +0300

    phoneNumber navigation to local calling app integrated

--- app/src/main/java/com/example/etransportapp/presentation/components/AdOwnerInfoSection.kt
+++ app/src/main/java/com/example/etransportapp/presentation/components/AdOwnerInfoSection.kt
@@ -1,10 +1,65 @@
 package com.example.etransportapp.presentation.components
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import com.example.etransportapp.ui.theme.RoseRed
 
 @Composable
 fun AdOwnerInfoSection(name: String, email: String, phone: String) {
-    InfoText("Ad Soyad", name)
-    InfoText("E-Posta", email)
-    InfoText("Telefon", phone)
+    val context = LocalContext.current
+
+    Column {
+        InfoText("Ad Soyad", name)
+        InfoText("E-Posta", email)
+
+        Text(
+            text = "Telefon",
+            color = Color.Gray, modifier = Modifier.padding(vertical = 4.dp)
+        )
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Phone,
+                contentDescription = "Telefon",
+                tint = RoseRed,
+                modifier = Modifier.size(16.dp)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = phone,
+                color = RoseRed,
+                fontWeight = FontWeight.Bold,
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier.clickable {
+                    val intent = Intent(Intent.ACTION_DIAL).apply {
+                        data = Uri.parse("tel:$phone")
+                    }
+                    context.startActivity(intent)
+                }.height(20.dp)
+            )
+        }
+
+
+        Spacer(modifier = Modifier.height(8.dp))
+    }
 }
