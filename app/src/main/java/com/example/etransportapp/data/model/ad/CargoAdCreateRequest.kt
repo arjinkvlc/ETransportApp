e2b8b0956d4f85cf74ebd36d70ed889395236170
Umut Kalelioğlu <72888847+umutkaleli@users.noreply.github.com>
2025-05-24 22:03:32 +0300

    CargoAdApi and models added

---
+++ app/src/main/java/com/example/etransportapp/data/model/ad/CargoAdCreateRequest.kt
@@ -0,0 +1,15 @@
+package com.example.etransportapp.data.model.ad
+
+data class CargoAdCreateRequest(
+    val userId: String,
+    val title: String,
+    val description: String,
+    val weight: Int,
+    val cargoType: String,
+    val dropCountry: String,
+    val dropCity: String,
+    val pickCountry: String,
+    val pickCity: String,
+    val currency: String,
+    val price: Int
+)
