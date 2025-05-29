ca1d33d81a276fd874889603da4b8dccb96b81cb
Umut Kalelioğlu <72888847+umutkaleli@users.noreply.github.com>
2025-05-29 09:47:18 +0300

    VehicleAd Create and Get response changed

--- app/src/main/java/com/example/etransportapp/data/remote/api/VehicleAdApi.kt
+++ app/src/main/java/com/example/etransportapp/data/remote/api/VehicleAdApi.kt
@@ -1,7 +1,6 @@
 package com.example.etransportapp.data.remote.api
 
 import com.example.etransportapp.data.model.ad.VehicleAdCreateRequest
-import com.example.etransportapp.data.model.ad.VehicleAdCreateResponse
 import com.example.etransportapp.data.model.ad.VehicleAdGetResponse
 import com.example.etransportapp.data.model.ad.VehicleAdUpdateRequest
 import retrofit2.Response
@@ -14,7 +13,7 @@ import retrofit2.http.Path
 
 interface VehicleAdApi {
     @POST("api/VehicleAd")
-    suspend fun createVehicleAd(@Body request: VehicleAdCreateRequest): Response<VehicleAdCreateResponse>
+    suspend fun createVehicleAd(@Body request: VehicleAdCreateRequest): Response<VehicleAdGetResponse>
 
     @GET("api/VehicleAd")
     suspend fun getAllVehicleAds(): Response<List<VehicleAdGetResponse>>
